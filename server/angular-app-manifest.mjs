0fbb46808ed4cc204737dd73716601c1c11efe16
edobertaz01 <121713000+edobertaz01@users.noreply.github.com>
2025-04-25 22:21:09 +0200

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -20,11 +20,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6275, hash: '4931ddba0c0d82402d2c20695fd940622a28ec684c7b9186ee3ed2dc05c53ff1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1005, hash: '69590c81e0c49ece1edf513b4e9f18f11829bdfc0f11c986a3d87ba145baa4a5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 12517, hash: 'c734933447ff2f64137474bbcea6f68762d4a4baff69452f7dd6ba46718461ff', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'contatti/index.html': {size: 14014, hash: 'e0d4eb50e57d92a27981c86a7dcd2ffd95d0a0c50288985c44311e340d26fd45', text: () => import('./assets-chunks/contatti_index_html.mjs').then(m => m.default)},
-    'chi-siamo/index.html': {size: 16381, hash: '9ec2c7a35bfe181de35fc667a740262f1e24dd12337501870ec7e5bdc644e5d2', text: () => import('./assets-chunks/chi-siamo_index_html.mjs').then(m => m.default)},
-    'styles.css': {size: 383897, hash: 'heJo7LrdeHM', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 6365, hash: 'e39deeec3069cde454f7cc92411758cbbba9d1468369f5d83ba96cec13e52976', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1032, hash: '86e2fefbc36afe797b536ae76ba5b50c7434926c42aac05355b301d4b5bc7c17', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'contatti/index.html': {size: 14104, hash: 'c5254e5db0ccfd7f03f6f709cbf61ec5fc382fd0338ff6a655599826945df54f', text: () => import('./assets-chunks/contatti_index_html.mjs').then(m => m.default)},
+    'chi-siamo/index.html': {size: 16471, hash: '034a5c4347cd8300312fc97fc1de040061b84b722a8b676074e24136daf11343', text: () => import('./assets-chunks/chi-siamo_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 12607, hash: '251fb3146414d3525a49368b2adbe4da30af9d6a6d0538eae853b94a3fa0ccf1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-EFPPUCV3.css': {size: 384095, hash: 'zbW8LDYxYXI', text: () => import('./assets-chunks/styles-EFPPUCV3_css.mjs').then(m => m.default)}
   },
 };
