d2ad06534df387bb878b1a634ec03a593dc5cb9b
edobertaz01 <121713000+edobertaz01@users.noreply.github.com>
2025-05-08 09:56:55 +0200

    update sito

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Dungeoncrawlerapp
+# LudoForgeApp
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.9.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'dungeoncrawlerapp' title`, () => {
+  it(`should have the 'LudoForgeApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dungeoncrawlerapp');
+    expect(app.title).toEqual('LudoForgeApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dungeoncrawlerapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, LudoForgeApp');
   });
 });
