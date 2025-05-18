30ca8861d711106b7e286657033eef4f52f2c82b
edobertaz01 <121713000+edobertaz01@users.noreply.github.com>
2025-05-18 19:59:23 +0200

    cookie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true, 
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'] 
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'LudoForgeApp';
+
+  showCookieBanner = true;
+  private platformId = inject(PLATFORM_ID);
+
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      const accepted = localStorage.getItem('cookiesAccepted');
+      this.showCookieBanner = accepted !== 'true' && accepted !== 'false'; // mostra solo se non ha ancora scelto
+    }
+  }
+
+  //al momento va bene ma in futuro accettane di più
+  accettaCookie() {
+    this.showCookieBanner = false;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('cookiesAccepted', 'true');
+    }
+    // qui potresti inizializzare Google Analytics, etc.
+  }
+
+  rejectCookies() {
+    this.showCookieBanner = false;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('cookiesAccepted', 'false');
+    }
+    // non attivare strumenti di tracciamento
+  }
 }
---
+++ src/pages/ad-banner/ad-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-banner',
+  imports: [],
+  templateUrl: './ad-banner.component.html',
+  styleUrl: './ad-banner.component.scss'
+})
+export class AdBannerComponent {
+
+}
